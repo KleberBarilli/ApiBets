2b3abceb2959c487aab792dbc6625be3c2065541
kleberbarilli <kleberbarilli73@gmail.com>
2021-08-25 18:49:34 -0300

    config de erros JOI

--- src/modules/bets/routes/bets.routes.ts
+++ src/modules/bets/routes/bets.routes.ts
@@ -1,13 +1,83 @@
 import { Router } from 'express';
 import BetsController from '../controllers/BetsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const betsRouter = Router();
 const betsController = new BetsController();
 
-betsRouter.get('/user/:id', betsController.index);
-betsRouter.get('/:id', betsController.show);
-betsRouter.post('/', betsController.create);
-betsRouter.put('/:id', betsController.update);
-betsRouter.delete('/:id', betsController.delete);
+betsRouter.get(
+	'/user/:id',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required(),
+		},
+	}),
+	betsController.index,
+);
+
+betsRouter.get(
+	'/:id',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required(),
+		},
+	}),
+	betsController.show,
+);
+
+betsRouter.post(
+	'/',
+	celebrate({
+		[Segments.BODY]: {
+			user_bet_id: Joi.string().uuid().required(),
+			event: Joi.string().required(),
+			bookie: Joi.string().required(),
+			bet: Joi.string().required(),
+			stake: Joi.number().required(),
+			odd: Joi.number().required(),
+			sport: Joi.string(),
+			tag: Joi.string(),
+			tipster: Joi.string(),
+			status: Joi.string(),
+			result: Joi.number(),
+			notes: Joi.string(),
+		},
+	}),
+	betsController.create,
+);
+
+betsRouter.put(
+	'/:id',
+	celebrate({
+		[Segments.BODY]: {
+			user_bet_id: Joi.string().uuid().required(),
+			event: Joi.string().required(),
+			bookie: Joi.string().required(),
+			bet: Joi.string().required(),
+			stake: Joi.number().required(),
+			odd: Joi.number().required(),
+			sport: Joi.string(),
+			tag: Joi.string(),
+			tipster: Joi.string(),
+			status: Joi.string(),
+			result: Joi.number(),
+			notes: Joi.string(),
+		},
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required(),
+		},
+	}),
+	betsController.update,
+);
+
+betsRouter.delete(
+	'/:id',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required(),
+		},
+	}),
+	betsController.delete,
+);
 
 export default betsRouter;
