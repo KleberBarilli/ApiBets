4168a997473141d83e0a05bee6635149a14ae572
kleberbarilli <kleberbarilli73@gmail.com>
2021-08-26 16:56:52 -0300

    sessions and auths

---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,49 @@
+import AppError from '@shared/errors/AppError';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import authConfig from '@config/auth';
+
+interface IUserRequest {
+	email: string;
+	password: string;
+}
+
+interface IResponse {
+	user: User;
+	token: string;
+}
+
+export default class CreateSessionsService {
+	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+	public async execute({
+		email,
+		password,
+	}: IUserRequest): Promise<IResponse> {
+		const userRepositories = getCustomRepository(UsersRepository);
+
+		const user = await userRepositories.findByEmail(email);
+
+		if (!user) {
+			throw new AppError('Incorret email/password', 401);
+		}
+
+		const passwordConfirmed = await compare(password, user.password);
+
+		if (!passwordConfirmed) {
+			throw new AppError('Incorret email/password', 401);
+		}
+
+		const token = sign({}, authConfig.jwt.secret, {
+			subject: user.id,
+			expiresIn: authConfig.jwt.expiresIn,
+		});
+
+		return {
+			user,
+			token,
+		};
+	}
+}
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import betsRouter from '@modules/bets/routes/bets.routes';
 import userRouter from '@modules/users/routes/users.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/bets', betsRouter);
 routes.use('/users', userRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
