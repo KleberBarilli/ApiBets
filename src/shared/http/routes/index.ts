51b99b04e6812ac955588e17efddfd943849e37c
kleberbarilli <kleberbarilli73@gmail.com>
2021-08-27 13:41:44 -0300

    forgot password token

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,11 +2,13 @@ import betsRouter from '@modules/bets/routes/bets.routes';
 import userRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import { Router } from 'express';
+import passwordRouter from '@modules/users/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/bets', betsRouter);
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
