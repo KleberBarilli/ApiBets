886cb541c36b597261b922f3cb6751c7d05c55ea
kleberbarilli <kleberbarilli73@gmail.com>
2021-09-01 22:36:04 -0300

    paginacao adicionada ao listar bets por usuario

--- src/modules/bets/services/ListBetsService.ts
+++ src/modules/bets/services/ListBetsService.ts
@@ -2,19 +2,31 @@ import { getCustomRepository } from 'typeorm';
 import Bet from '../typeorm/entities/Bet';
 import BetsRepository from '../typeorm/repositories/BetsRepository';
 
+interface IPaginateBets {
+	from: number;
+	to: number;
+	per_page: number;
+	total: number;
+	current_page: number;
+	prev_page: number | null;
+	next_page: number | null;
+	data: Bet[];
+}
+
 export default class ListBetsService {
-	async execute(user_id: string): Promise<Bet[]> {
+	async execute(user_id: string): Promise<IPaginateBets> {
 		const betsRepositories = getCustomRepository(BetsRepository);
 
-		const bets = await betsRepositories.find({
-			where: {
+		const bets = await betsRepositories
+			.createQueryBuilder()
+			.where({
 				user_bet_id: user_id,
-			},
-			order: {
+			})
+			.orderBy({
 				date: 'DESC',
-			},
-		});
+			})
+			.paginate();
 
-		return bets;
+		return bets as IPaginateBets;
 	}
 }
