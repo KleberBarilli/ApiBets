685bffcf33f8ee5118f4682ee9ca86d7fe45fe4b
kleberbarilli <kleberbarilli73@gmail.com>
2021-09-01 22:59:48 -0300

    class transformer, exclude password expose avatarurl

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import CreateUserService from '../services/CreateUserService';
 import ListUserService from '../services/ListUserService';
+import { classToClass } from 'class-transformer';
 
 export default class UsersController {
 	public async index(req: Request, res: Response): Promise<Response> {
@@ -9,7 +10,7 @@ export default class UsersController {
 		const users = await listUser.execute();
 		console.log(req.user.id);
 
-		return res.json(users);
+		return res.json(classToClass(users));
 	}
 
 	public async create(req: Request, res: Response): Promise<Response> {
@@ -23,6 +24,6 @@ export default class UsersController {
 			password,
 		});
 
-		return res.json(user);
+		return res.json(classToClass(user));
 	}
 }
