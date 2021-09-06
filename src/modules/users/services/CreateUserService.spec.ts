f7de5200bdf42ab6346194801689c73341e41a12
kleberbarilli <kleberbarilli73@gmail.com>
2021-09-06 18:21:29 -0300

    testes sessions

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -2,14 +2,20 @@ import 'reflect-metadata';
 import CreateUserService from './CreateUserService';
 import FakeUsersRepository from '@modules/users/domain/repositories/fakes/FakeUsersRepository';
 import AppError from '@shared/errors/AppError';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 let fakeUsersRepository: FakeUsersRepository;
 let createUserService: CreateUserService;
+let fakeHashProvider: FakeHashProvider;
+
 describe('CreateUser', () => {
 	beforeEach(() => {
 		fakeUsersRepository = new FakeUsersRepository();
-
-		createUserService = new CreateUserService(fakeUsersRepository);
+		fakeHashProvider = new FakeHashProvider();
+		createUserService = new CreateUserService(
+			fakeUsersRepository,
+			fakeHashProvider,
+		);
 	});
 	it('Should be able to create a new user', async () => {
 		const user = await createUserService.execute({
@@ -37,10 +43,12 @@ describe('CreateUser', () => {
 		).rejects.toBeInstanceOf(AppError);
 	});
 
-	it('Should not be able to create two users with the same username', () => {
-		const fakeUsersRepository = new FakeUsersRepository();
-
-		const createUserService = new CreateUserService(fakeUsersRepository);
+	it('Should not be able to create two users with the same username', async () => {
+		await createUserService.execute({
+			username: 'kleber teste',
+			email: 'kleber@teste.com',
+			password: '123123',
+		});
 		expect(
 			createUserService.execute({
 				username: 'kleber teste',
